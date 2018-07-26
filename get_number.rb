4d2ea2d57237b2ac77e0837540273e89d65b2319
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-07-26 14:40:08 -0500

    generated random number

--- get_number.rb
+++ get_number.rb
@@ -5,4 +5,15 @@ puts "Welcome to 'Get My Number!'"
 print "What's your name? "
 input = gets
 
-puts "Welcome, #{input}!"
+name = input.chomp
+
+puts "Welcome, #{name}!"
+
+#Store a random number for the player to guess.
+
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
+
+
+
