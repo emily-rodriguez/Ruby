8bd157659b37497f75b1cbd24e990c923e37ac70
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-07-27 16:39:50 -0500

    end of chapter 4

--- employee.rb
+++ employee.rb
@@ -9,6 +9,10 @@ class Employee
     @name = name
   end
 
+  def initialize(name = "Anonymous")
+    self.name = name
+  end
+
   def print_name
     puts "Name: #{name}"
   end
@@ -26,6 +30,11 @@ class SalariedEmployee < Employee
     @salary = salary
   end
 
+  def initialize(name = "Anonymous", salary = 0.0)
+    super(name)
+    self.salary = salary
+  end
+
   def print_pay_stub
     print_name
     pay_for_period = format("%.2f", (salary/365.0) * 14)
@@ -35,6 +44,19 @@ class SalariedEmployee < Employee
 end
 
 class HourlyEmployee < Employee
+
+  def self.security_guard(name)
+    HourlyEmployee.new(name, 19.25, 30)
+  end
+
+  def self.cashier(name)
+    HourlyEmployee.new(name, 12.75, 25)
+  end
+
+  def self.janitor(name)
+    HourlyEmployee.new(name, 10.50, 20)
+  end
+
   attr_reader :hourly_wage, :hours_per_week
 
   def hourly_wage=(hourly_wage)
@@ -46,10 +68,17 @@ class HourlyEmployee < Employee
 
   def hours_per_week=(hours_per_week)
     if hours_per_week < 0
-      raise "A hourly wage of #{hours_per_week} isn't valid!"
+      raise "#{hours_per_week} hours per week isn't valid!"
     end
     @hours_per_week = hours_per_week
   end
+
+  def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+    super(name)
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
+  end
+
 
   def print_pay_stub
     print_name
@@ -60,8 +89,17 @@ class HourlyEmployee < Employee
 
 end
 
-hourly = HourlyEmployee.new
-hourly.name = "John Smith"
-hourly.hourly_wage = 14.97
-hourly.hours_per_week = 30
+salaried = SalariedEmployee.new("Jane Doe", 50000)
+salaried.print_pay_stub
+
+hourly = HourlyEmployee.new("John Smith", 14.97, 30)
 hourly.print_pay_stub
+
+angela = HourlyEmployee.security_guard("Angela Matthews")
+edwin = HourlyEmployee.janitor("Edwin Burgess")
+ivan = HourlyEmployee.cashier("Ivan Stokes")
+
+angela.print_pay_stub
+edwin.print_pay_stub
+ivan.print_pay_stub
+
