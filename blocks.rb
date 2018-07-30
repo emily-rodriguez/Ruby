6c8e98d0c42d0181b75303b5f0ec12c5e1c363c8
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-07-30 15:34:45 -0500

    refactored code to use .each

--- blocks.rb
+++ blocks.rb
@@ -8,3 +8,16 @@ my_method do
   puts "We're in the block!"
 end
 
+def take_this
+  yield "present"
+end
+
+take_this do |thing|
+  puts "do/end block got #{thing}"
+end
+
+take_this { |thing| puts "braces block got #{thing}"}
+
+alphabet = ["a", "b", "c"]
+
+alphabet.each { |param| puts param}
