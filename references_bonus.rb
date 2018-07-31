98b5ecb2964e4b4bbc221cb3dd53403221547551
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-07-31 15:02:24 -0500

    finished with chaper 8, including extra exercises

---
+++ references_bonus.rb
@@ -0,0 +1,12 @@
+staff = ["Walsh", "Moreno", "Nash", "Cox", "Rios", "Jones", "Gupta", "Hayes"]
+softball_team = staff.clone
+
+# .clone method (not covered in the book) creates a copy of the array
+
+staff << "Wagner"
+softball_team << "Zonky the Wonder Squirrel"
+
+puts "Company staff:", staff
+print "\n\n"
+puts "Company softball team:", softball_team
+
