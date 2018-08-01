ae1c9cbb538498ac6682a67356923c7355622fc9
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-08-01 17:21:18 -0500

    movie app exercise complete

--- movies/app.rb
+++ movies/app.rb
@@ -1,15 +1,11 @@
 require "sinatra"
 require "movie"
-require "yaml"
+require "movie_store"
+
+store = MovieStore.new("movies.yml")
 
 get ("/movies") do
-  @movies = []
-  @movies[0] = Movie.new
-  @movies[0].title = "Jaws"
-  @movies[1] = Movie.new
-  @movies[1].title = "Alien"
-  @movies[2] = Movie.new
-  @movies[2].title = "Terminator 2"
+  @movies = store.all
   erb :index
 end
 
@@ -17,9 +13,19 @@ get ("/movies/new") do
   erb :new
 end
 
-post("/movies/create") do
+post ("/movies/create") do
   @movie = Movie.new
   @movie.title = params["title"]
   @movie.director = params["director"]
   @movie.year = params["year"]
+  store.save(@movie)
+  redirect "/movies/new"
 end
+
+get ("/movies/:id") do
+  id = params["id"].to_i
+  @movie = store.find(id)
+  erb :show
+end
+
+
