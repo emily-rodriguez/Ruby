b7d1837f5bbbf1e61c630872d80d46d6da9da69c
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-08-01 14:02:40 -0500

    finished with exceptions, including extra exercises

---
+++ exceptions_bonus.rb
@@ -0,0 +1,47 @@
+def trip_over_curb
+  raise "WHOA!"
+end
+
+begin
+  trip_over_curb
+rescue
+  puts "Caught you!"
+end
+
+def send_sos
+  raise "Latitude -50.75, Longitude 166.04"
+end
+
+begin
+  send_sos
+rescue => error
+  puts "Sending rescue party to: #{error.message}"
+end
+
+class ThisError < StandardError
+end
+
+class ThatError < StandardError
+end
+
+class TheOtherError < StandardError
+end
+
+def random_failure
+  number = rand(0..2)
+  if number == 0
+    raise ThisError, "Whoops!"
+  elsif number == 1
+    raise ThatError, "Uh, oh!"
+  elsif number == 2
+    raise TheOtherError, "Oh, no!"
+  end
+end
+
+begin
+  random_failure
+rescue ThisError
+  puts "I caught this!"
+rescue ThatError
+  puts "I caught that!"
+end
---
+++ small_oven.rb
@@ -0,0 +1,47 @@
+class OvenOffError < StandardError
+end
+
+class OvenEmptyError < StandardError
+end
+
+class SmallOven
+
+  attr_accessor :contents
+
+  def turn_on
+    puts "Turning oven on."
+    @state = "on"
+  end
+
+  def turn_off
+    puts "Turning oven off."
+    @state = "off"
+  end
+
+  def bake
+    unless @state == "on"
+      raise OvenOffError, "You need to turn the oven on first!"
+    end
+    if @contents == nil
+      raise OvenEmptyError, "There's nothing in the oven!"
+    end
+    "Serving golden-brown #{contents}"
+  end
+end
+
+dinner = ["turkey", nil, "pie"]
+oven = SmallOven.new
+oven.turn_off
+dinner.each do |item|
+  begin
+    oven.contents = item
+    puts oven.bake
+  rescue OvenEmptyError => error
+    puts "Error: #{error.message}"
+  rescue OvenOffError => error
+    oven.turn_on
+    retry
+  ensure
+    oven.turn_off
+  end
+end
