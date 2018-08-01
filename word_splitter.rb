82ff98d3a3b2fbb3f4829adf5725f03c7f578d11
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-08-01 10:18:52 -0500

    added comments to word_splitter.rb to create html documentation

--- word_splitter.rb
+++ word_splitter.rb
@@ -1,19 +1,26 @@
+#This class allows you to perform various operations on the words in a string.
 class WordSplitter
 
   include Enumerable
 
+  #The string to split into words.
   attr_accessor :string
 
+  #Passes each word in the string to a block, one at a time.
   def each
     string.split(" ").each do |word|
       yield word
     end
   end
 
+  #Creates a new instance with its string attribute set to the given string.
+  def initialize(string)
+    self.string = string
+  end
+
 end
 
-splitter = WordSplitter.new
-splitter.string = "how do you do"
+splitter = WordSplitter.new("how do you do")
 
 p splitter.find_all {|word| word.include?("d")}
 p splitter.reject {|word| word.include?("d")}
